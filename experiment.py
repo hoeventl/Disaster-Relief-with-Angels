afc9fea605cf3f87cec11695965645db111f1bc6
HOEVENTL <hoeventl@rose-hulman.edu>
2024-04-03 11:03:53 -0500

    usage changes

--- experiment.py
+++ experiment.py
@@ -9,21 +9,38 @@ angel_demand_vals = [i for i in range(10,51,5)]
 activation_cost_vals = [w for w in range(10,91,10)]
 connectivity_vals = [1, 0.9, 0.75, 0.5, 0.3]
 
-run(OUTPUT_FOLDER+SUBFOLDER, 
-    Network("./Instances/A-n33-k6.vrp",
-            num_angels=3,
-            radius=[20, 30, 25],
-            aid=16,
-            angel_locs=[(45,55), (70,70), (10,10)],
-            angel_demand=40),
-    "u1")
+# run(OUTPUT_FOLDER+SUBFOLDER, 
+#     Network("./Instances/A-n33-k6.vrp",
+#             num_angels=3,
+#             radius=[20, 30, 25],
+#             aid=16,
+#             angel_locs=[(45,55), (70,70), (10,10)],
+#             angel_demand=40),
+#     "u1")
 
+# run(OUTPUT_FOLDER+SUBFOLDER, 
+#     Network("./Instances/P-n55-k15.vrp",
+#             num_angels=3,
+#             radius=20,
+#             aid=22,
+#             angel_locs=[(20,30), (20,60), (60,40)],
+#             angel_demand=50),
+#     "u2")
+
+# run(OUTPUT_FOLDER+SUBFOLDER, 
+#     Network("./Instances/P-n55-k15.vrp",
+#             num_angels=3,
+#             radius=20,
+#             aid=19,
+#             angel_locs=[(20,30), (20,60), (60,40)],
+#             angel_demand=50),
+#     "u3")
 
 # clear_folder(OUTPUT_FOLDER+SUBFOLDER)
 # variable_radius(INSTANCE, OUTPUT_FOLDER+SUBFOLDER, suffix, radius_vals)
 # variable_angel_demand(INSTANCE, OUTPUT_FOLDER+SUBFOLDER, suffix, angel_demand_vals)
 # variable_activation_cost(INSTANCE, OUTPUT_FOLDER+SUBFOLDER, suffix, activation_cost_vals)
 # variable_connectivity(INSTANCE, OUTPUT_FOLDER+SUBFOLDER, suffix, connectivity_vals, 10)
-analyze_solutions(OUTPUT_FOLDER+SUBFOLDER)
+# analyze_solutions(OUTPUT_FOLDER+SUBFOLDER)
 
 visualize_experiments(OUTPUT_FOLDER+SUBFOLDER, ["u1"])
